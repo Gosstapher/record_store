ff32009e7b1b8d5a383125218fd820abd612df69
Gosstapher <gosstapher@gmail.com>
2016-01-30 13:17:46 +0000

    wasted time fixing stupid mistakes

--- test.js
+++ test.js
@@ -14,4 +14,24 @@ describe('Record', function(){
     var record1 = new Record('Tom Jones', 'Praise and Blame', 4.99);
     assert.equal(4.99, record1.price)
   });
+  it('should be possible to create multiple records', function(){
+    var record1 = new Record('Tom Jones', 'Praise and Blame', 4.99);
+    var record2 = new Record('Con Bro Chill', 'We Came to Party', 7.99);
+    var record3 = new Record('Zoë Keating', 'Into The Trees', 14.99);
+    assert.equal('Tom Jones', record1.artist);
+    assert.equal('Con Bro Chill', record2.artist);
+    assert.equal('Zoë Keating', record3.artist);
+    });
 });
+
+
+
+
+
+
+
+
+
+
+
+
